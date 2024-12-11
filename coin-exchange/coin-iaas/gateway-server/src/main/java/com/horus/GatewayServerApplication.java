b6dd913d358b9b3ceee9d628965e91d9228a081f
horus <xiaoshewan@126.com>
2024-12-11 22:10:09 +0800

    [fix] modify GatewayServer config

--- coin-exchange/coin-iaas/gateway-server/src/main/java/com/horus/GatewayServerApplication.java
+++ coin-exchange/coin-iaas/gateway-server/src/main/java/com/horus/GatewayServerApplication.java
@@ -1,9 +1,11 @@
 package com.horus;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@Slf4j
 @SpringBootApplication
 // 服务发现
 @EnableDiscoveryClient
